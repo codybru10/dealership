9255f7c7e63d2236bf7d7433b142f08919defe2a
Cody Brubaker and Tracie Weitzman <tracie@>
2016-11-30 14:01:17 -0800

    finish integration testing

--- spec/dealership_intergration_spec.rb
+++ spec/dealership_intergration_spec.rb
@@ -34,7 +34,25 @@ describe('the vehicles added to dealership page file path', {:type => :feature})
   it('processes the user click and returns list of vehicles') do
     visit('/dealerships')
     click_link('Cars Cars Cars')
-    expect(page).to have_content('Here are all the cars in this dealership:
-')
+    expect(page).to have_content('Here are all the cars in this dealership:')
+  end
+end
+
+describe('adds vehicles to dealership', {:type => :feature}) do
+  it('processes the user input and adds to dealership vehicle list') do
+    visit('/dealerships/1/vehicles/new')
+    fill_in('make', :with => 'Toyota')
+    fill_in('model', :with => 'Camry')
+    fill_in('year', :with => 2001)
+    click_button('Add Vehicle')
+    expect(page).to have_content('Success')
+  end
+end
+
+describe('vehicle details path', {:type => :feature}) do
+  it('opens new page for each listed vehicle within dealership') do
+    visit('/dealerships/1')
+    click_link('Toyota Camry 2001')
+    expect(page).to have_content('Toyota')
   end
 end
